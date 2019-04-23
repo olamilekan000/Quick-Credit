93290373fde60700829eaf4c31b98eccced47b4b
Olamilekan Odukoya <olarpringo@gmail.com>
2019-04-23 13:01:37 +0100

    #[165515143] Webpack config

---
+++ UI/webpack.dev.js
@@ -0,0 +1,37 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    index: './src/index.js',
+    signin: './src/signin.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env']
+        }
+      }
+    ]
+  },
+  devServer: {
+    port: 8085
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      inject: true,
+      chunks: ['index'],
+      filename: 'index.html'
+    }),
+    new HtmlWebpackPlugin({
+      template: './client/signin.html',
+      inject: true,
+      chunks: ['signin'],
+      filename: 'signin.html'
+    }),    
+  ]
+};
